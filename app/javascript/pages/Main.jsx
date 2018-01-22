664ff258455ced9fe70bee862a88c77ce9b61996
Luke McDonald <t.luke.mcdonald@gmail.com>
2018-01-22 17:28:24 -0500

    sidebar

--- app/javascript/pages/Main.jsx
+++ app/javascript/pages/Main.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import Aside from '../components/Aside';
 import "./../../assets/stylesheets/Main"
 
 const Main = (props) => {
   console.log('Main component loaded');
   return (
     <div className="main">
-      <div className='asideFrame'>
-        <Aside/>
-      </div>
-      <div className='mainFrame'>
-        <h1> Main </h1>
-      </div>
-
+    <h1>Welcome to my site </h1>
     </div>
   );
 };
