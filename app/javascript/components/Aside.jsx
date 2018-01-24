e8eb2f85328fd8daf14db5a7b3dabe3e2712ef49
Luke McDonald <t.luke.mcdonald@gmail.com>
2018-01-23 22:56:31 -0500

    css sidebar

--- app/javascript/components/Aside.jsx
+++ app/javascript/components/Aside.jsx
@@ -1,65 +1,62 @@
 import React from 'react';
 import "./../../assets/stylesheets/aside"
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-
+import Arrow from "./../../assets/images/double-right.svg"
 
 const Aside = (props) => {
 console.log('Aside component loaded');
 
   return (
+    <div className="sidebar bg-light" id="sidebar">
 
-    <div className='aside'>
+      <div className="bg-light b1">
+        <img className="arrow" src={Arrow} alt=">" />
+      </div>
 
-      <div className="sidebar" id="sidebar">
-      <button id="b1"> -></button>
-
-      <div className="form-check">
+      <div className="form-check filterframe">
         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
         <label className="form-check-label" htmlFor="defaultCheck1">
           Select All
         </label>
       </div>
-      <div className="form-check">
+      <div className="form-check filterframe">
         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
         <label className="form-check-label" htmlFor="defaultCheck1">
           Head
         </label>
       </div>
-      <div className="form-check">
+      <div className="form-check filterframe">
         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
         <label className="form-check-label" htmlFor="defaultCheck1">
           Arms
         </label>
       </div>
-      <div className="form-check">
+      <div className="form-check filterframe">
         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
         <label className="form-check-label" htmlFor="defaultCheck1">
           Back
         </label>
       </div>
-      <div className="form-check">
+      <div className="form-check filterframe">
         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
         <label className="form-check-label" htmlFor="defaultCheck1">
           Torso
         </label>
       </div>
-      <div className="form-check">
+      <div className="form-check filterframe">
         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
         <label className="form-check-label" htmlFor="defaultCheck1">
           Legs
         </label>
       </div>
-      <div className="form-check">
+      <div className="form-check filterframe">
         <input className="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
         <label className="form-check-label" htmlFor="defaultCheck1">
           Internal
         </label>
       </div>
+    </div>
 
-      <div className="wrapper">
-      </div>
-    </div>
-    </div>
   );
 };
 
